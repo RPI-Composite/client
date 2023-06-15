cfe54e622c6d5a7e6a2e959587014ab97873f305
EmilioBejasa <69165764+EmilioBejasa@users.noreply.github.com>
2023-06-15 10:39:55 -0400

    Update AboutUs.js
    Putting our names in the About Me page for now, will update descriptions later.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,7 +8,11 @@ function AboutUs() {
         <h1>About RPI Composite</h1>
             <div className="headerSection">
                 <h2>Who developed RPI Composite</h2>
-                <p>[About us Section]</p>
+                <p><b>Itamar Oren-Naftalovich</b>: Junior Computer Science Student at RPI.</p>
+                <p><b>Ben Manicke</b>: Junior Computer Science Student at RPI.</p>
+                <p><b>Chris Bejasa</b>: Junior Computer Science Student at RPI.</p>
+                <p><b>Siddhant Agarwal</b>: Junior Computer Science Student at RPI. Working in Full Stack Development of the website.</p>
+                <p><b>Albert Bao</b>: Junior Computer Science Student at RPI.</p>
             </div>
             <div className="headerSection">
                 <h2>What Comes Next?</h2>
