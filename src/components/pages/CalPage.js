697382e2b414f2f13e29c6ee330db24b57746089
Haoran-Bao <albert_bao@hotmail.com>
2023-06-28 15:53:12 -0400

    "trying to fetch data from the backend"

--- src/components/pages/CalPage.js
+++ src/components/pages/CalPage.js
@@ -9,37 +9,50 @@ import interactionPlugin from "@fullcalendar/interaction";
 
 
 
+
 function CalPage() {
-  const eventList = [
-    {
-      title: 'Event 1',
-      start: '2023-06-20T10:00:00',
-      end: '2023-06-20T12:00:00',
-      description: 'This is Event 1'
-    },
-    {
-      title: 'Event 2',
-      start: '2023-06-23T15:00:00',
-      end: '2023-06-23T17:00:00',
-      description: 'This is Event 2'
-    },
-    // Add more events as needed
-  ];
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get('/acalRaw'); // Make a GET request to the server's /acalRaw endpoint
-        const formattedEvents = response.data; // Assume the response from the server is already in the required format
-        setEvents(formattedEvents);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    fetchData();
+    fetchEvents();
   }, []);
+
+  const fetchEvents = () => {
+    axios.get('/scripts/academic-calendar.js')
+      .then(response => {
+        const eventData = response.data;
+
+        const parsedEvents = [];
+
+        for (const key in eventData) {
+          if (eventData.hasOwnProperty(key)) {
+            const monthYear = key;
+            const eventsForMonth = eventData[key];
+
+            for (const date in eventsForMonth) {
+              if (eventsForMonth.hasOwnProperty(date)) {
+                const event = eventsForMonth[date];
+
+                const eventObj = {
+                  monthYear: monthYear,
+                  date: date,
+                  txt: event.txt,
+                  href: event.href
+                };
+
+                parsedEvents.push(eventObj);
+              }
+            }
+          }
+        }
+
+        setEvents(parsedEvents);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+  
 
   return (
     <div>
@@ -54,7 +67,7 @@ function CalPage() {
             end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
           }}
           height={"90vh"}
-          events={eventList}
+          events={events}
         />
       </div>
       {/* Side Bar */}
