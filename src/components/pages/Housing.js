cf613bd32f19c5b4059db176473696c7d453e070
ION606 <orenni@rpi.edu>
2023-07-17 17:13:17 -0400

    added main housing directory

--- src/components/pages/Housing.js
+++ src/components/pages/Housing.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../../App.css';
+import '../Dorm.css';
 
 
 function Housing() {
@@ -20,23 +21,30 @@ function Housing() {
     }
   }
 
-
-  return (
-    <>
-      <h1>Housing Page</h1>
-      <div>
-        <h2>Dorms</h2>
-        {Object.keys(dormInfo).map((dormKey) => (
-          <div key={dormKey}>
-            <h3>{dormInfo[dormKey].name}</h3>
+{/* <h3>{dormInfo[dormKey].name}</h3>
             <p>Cohort: {dormInfo[dormKey].cohort}</p>
             <p>
               URL: <a href={"http://localhost:3001/housing/" + dormKey}>site URL</a>
-            </p>
-          </div>
-        ))}
+            </p> */}
+  return (
+    <div className='bodyMain'>
+      <h1>Housing Page</h1>
+      <div>
+        <h2>Dorms</h2>
+        <table>
+          <tbody>
+            {Object.keys(dormInfo).map((dormKey) => {
+              return (
+                <tr>
+                  <th>{dormInfo[dormKey]['cohort']}</th>
+                  <th><a className='housingURL' href={"http://localhost:3001/housing/" + dormKey}>{dormInfo[dormKey].name}</a></th>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
-    </>
+    </div>
   );
 }
 
