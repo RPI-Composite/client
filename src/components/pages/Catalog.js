8d2a91a265504ddf8a3a1132ea0db38da134211a
Ben Manicke <manicb@rpi.edu>
2023-07-18 12:06:56 -0400

    Updating Catalog.js
    Just adding some content to the page. This will allow us to populate our catalog page with content.

--- src/components/pages/Catalog.js
+++ src/components/pages/Catalog.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../../App.css';
 
 function Catalog() {
+  const [schoolsData, setSchoolsData] = useState([]);
+
+  useEffect(() => {
+    fetchSchoolsData();
+  }, []);
+
+  const fetchSchoolsData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/schools');
+      const data = response.data;
+      setSchoolsData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-        <h1>Catalog</h1>
+      <h1>Hello World</h1>
+      <h2>Schools:</h2>
+      {schoolsData.map((school, index) => (
+        <div key={index}>
+          <h3>{school.name}</h3>
+          <ul>
+            {school.depts.map((dept, index) => (
+              <li key={index}>{dept.name}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
