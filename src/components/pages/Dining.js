e148fe8fa3831a3984b1fe05b4e5f3f20f67bdea
SpadeZap <spadezap@gmail.com>
2023-07-24 12:05:06 -0400

    Made formatting better, added google map locations and phone number format, and Sodexo links.

--- src/components/pages/Dining.js
+++ src/components/pages/Dining.js
@@ -46,6 +46,38 @@ function Dining() {
     });
   };
 
+  const formatPhoneNumber = (phoneNumber) => {
+    const cleaned = ('' + phoneNumber).replace(/\D/g, '');
+    const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+      return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+    }
+    return phoneNumber;
+  };
+
+  const getLocationLink = (latitude, longitude) => {
+    return `https://www.google.com/maps?q=${latitude},${longitude}`;
+  };
+
+  // Additional links for each dining hall
+  const diningHallLinks = {
+    "Moe's Southwest Grill": 'https://rpi.sodexomyway.com/dining-near-me/moes',
+    'DCC Café': 'https://rpi.sodexomyway.com/dining-near-me/dcc-cafe',
+    "Evelyn's Cafe": 'https://rpi.sodexomyway.com/dining-near-me/evelyns_cafe',
+    'Argo Tea Café': 'https://rpi.sodexomyway.com/dining-near-me/library-cafe',
+    "Father's Marketplace": 'https://rpi.sodexomyway.com/dining-near-me/fathers-marketplace',
+    'Panera Bread': 'https://rpi.sodexomyway.com/dining-near-me/mcneil-room',
+    'Beanery Café': 'https://rpi.sodexomyway.com/dining-near-me/beanery-cafe',
+    'Blitman Commons': 'https://rpi.sodexomyway.com/dining-near-me/blitman-dining-hall',
+    'BARH Dining Hall': 'https://rpi.sodexomyway.com/dining-near-me/barh-dining-hall',
+    'Russell Sage Dining Hall': 'https://rpi.sodexomyway.com/dining-near-me/russell-sage',
+    'Commons Dining Hall': 'https://rpi.sodexomyway.com/dining-near-me/commons-dining-hall',
+    'RPI McNeil Room': 'https://rpi.sodexomyway.com/dining-near-me/mcneil-room',
+    'Wild Blue Sushi': 'https://rpi.sodexomyway.com/dining-near-me/wild_blue_sushi',
+    'Center for Biotechnology and Interdisciplinary Studies': 'https://rpi.sodexomyway.com/dining-near-me/CCG',
+    'Halal Shack': 'https://rpi.sodexomyway.com/dining-near-me/Halal_shack',
+  };
+
   return (
     <div className="bodyMain">
       <h1 className="titleMain">Dining Halls</h1>
@@ -56,13 +88,32 @@ function Dining() {
               <h1>{category}</h1>
               {Object.entries(halls).map(([hall, info], index) => (
                 <div key={hall} className={`diningHallSection ${index % 2 === 0 ? 'even' : 'odd'}`}>
-                  <h3 className="diningHallName">{hall.toUpperCase()}</h3>
+                  {diningHallLinks[hall] ? (
+                    <h3 className="diningHallName">
+                      <a href={diningHallLinks[hall]}>{hall.toUpperCase()}</a>
+                    </h3>
+                  ) : (
+                    <h3 className="diningHallName">{hall.toUpperCase()}</h3>
+                  )}
                   {info.desc && <p className="diningHallDescription"><span style={{textDecoration: 'underline'}}>Description:</span><br />{info.desc}</p>}
-                  {info.phone && <p>Phone: <a href={"tel:+1" + info.phone}>{info.phone}</a></p>}
+                  {info.phone && <p>Phone: <a href={"tel:+1" + info.phone}>{formatPhoneNumber(info.phone)}</a></p>}
                   {info.loc && (
                     <p>
-                      Location: <span style={{fontFamily: "-moz-initial"}}>Latitude: {info.loc.lat}, Longitude: {info.loc.long}</span>
+                      <a href={getLocationLink(info.loc.lat, info.loc.long)} style={{fontFamily: "-moz-initial"}}>
+                        Location:</a>
                     </p>
+                  )}
+                  {mealPrices && mealPrices[hall] && (
+                    <div>
+                      <p>Meal Prices:</p>
+                      <ul>
+                        {Object.entries(mealPrices[hall]).map(([meal, prices]) => (
+                          <li key={meal}>
+                            {meal}: {prices}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   )}
                   {info.reghours && (
                     <div>
@@ -82,22 +133,19 @@ function Dining() {
                               </li>
                             )
                           }
-                          else if (hall !== "Commons") { // Format regular hours for all halls except "Commons"
-                            return (
-                              <span className="hoursContainer" key={days}>
-                                <p style={{display: "inline-block"}}>{days.replaceAll(',', ', ')}:</p>
-                                <p style={{display: "inline-block", fontFamily: "-moz-initial"}}>&nbsp;&nbsp;{hours}</p>
-                                <br />
-                              </span>
-                            );
-                          }
-                          return null; // Skip displaying regular hours for "Commons"
+                          // Display regular hours as a single list item for halls with simple strings for hours
+                          return (
+                            <li key={days} className="hoursContainer">
+                              <p>{days.replaceAll(',', ', ')}:</p>
+                              <p style={{ fontFamily: "-moz-initial" }}>{hours}</p>
+                            </li>
+                          );
                         })}
                       </ul>
+                      
                     </div>
                   )}
                   {/* Add more information as needed */}
-                  {/* ... (existing JSX code) */}
                 </div>
               ))}
             </div>
@@ -108,6 +156,7 @@ function Dining() {
       )}
     </div>
   );
+  
 };
 
 export default Dining;
