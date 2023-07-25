ef65d86149930227936e029d388d452ba307ab02
EmilioBejasa <69165764+EmilioBejasa@users.noreply.github.com>
2023-07-25 14:14:34 -0400

    Changed OtherHelp
    Embedded the pdfs instead of giving the links, change the links hover and clicked color, changed navbar justification to left to shift RPI Composite to the left.

--- src/components/pages/OtherHelp.js
+++ src/components/pages/OtherHelp.js
@@ -1,22 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../../App.css';
+import '../OtherHelp.css';
 
 function OtherHelp() {
   const disorientation_pdf_path = "./RPI_Disorientation_Guide.pdf";
   return (
+    
     <div>
-      <h1>Campus Map and Disorientation Guide</h1>
-      <p><a href="./PDFs/RPI_Campus_Map.pdf">Campus Map</a></p>
-      <p><a href="./PDFs/RPI_Disorientation_Guide.pdf">Disorientation Guide</a></p>
+      <div>
+        <h1>Campus Map and Disorientation Guide</h1>
+        <object
+        data="./PDFs/RPI_Campus_Map.pdf"
+        type="application/pdf"
+        width="100%"
+        height="500px"
+        >
+        <p>
+          Your browser does not support PDF viewing. You can view the campus map PDF <a href="./PDFs/RPI_Campus_Map.pdf">here</a>.
+        </p>
+        </object>
+
+        <object
+        data="./PDFs/RPI_Disorientation_Guide.pdf"
+        type="application/pdf"
+        width="100%"
+        height="500px"
+        >
+        <p>
+          Your browser does not support PDF viewing. You can view the disorientation guide PDF <a href="./PDFs/RPI_Disorientation_Guide.pdf">here</a>.
+        </p>
+        </object>
+      </div>
+
+      <div>
       <h1>Other Useful Links:</h1>
-      <p><a href="https://hasspathways.com/">Hass Pathways</a></p>
-      <p><a href="https://info.rpi.edu/campus-card/">Check your Flex/RAD Balance here</a></p>
-      <p><a href="https://dotcio.rpi.edu/services/support-and-training/help-desk">Help Desk</a></p>
-      <p><a href="https://itssc.rpi.edu/hc/en-us">IT Services</a></p> 
-      <p><a href="https://rpiathletics.com/">Athletics</a></p> 
+        <div className='usefulLinks'> 
+          <p><a href="https://hasspathways.com/">Hass Pathways</a></p>
+          <p><a href="https://info.rpi.edu/campus-card/">Check your Flex/RAD Balance here</a></p>
+          <p><a href="https://dotcio.rpi.edu/services/support-and-training/help-desk">Help Desk</a></p>
+          <p><a href="https://itssc.rpi.edu/hc/en-us">IT Services</a></p> 
+          <p><a href="https://rpiathletics.com/">Athletics</a></p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default OtherHelp;
