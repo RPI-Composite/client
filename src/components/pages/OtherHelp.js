8041cd7de22c76b5632b102b7a805cc986d0f085
SpadeZap <spadezap@gmail.com>
2023-07-28 11:09:32 -0400

    Added more links and edited OtherHelp.js format

--- src/components/pages/OtherHelp.js
+++ src/components/pages/OtherHelp.js
@@ -9,6 +9,20 @@ function OtherHelp() {
     
     <div>
       <div>
+      <h1><u>Other Useful Links</u></h1>
+        <div className='usefulLinks'> 
+          <p><a href="https://lms.rpi.edu/"target="_blank">RPI Learning Management System</a></p>
+          <p><a href="https://sis.rpi.edu/"target="_blank">Self-Service Information System</a></p>
+          <p><a href="https://info.rpi.edu/bias-assessment-response-team/resources"target="_blank">Student Health Resources</a></p>
+          <p><a href="https://hasspathways.com/"target="_blank">Hass Pathways</a></p>
+          <p><a href="https://info.rpi.edu/campus-card/"target="_blank">Check your Flex/RAD Balance here</a></p>
+          <p><a href="https://dotcio.rpi.edu/services/support-and-training/help-desk"target="_blank">Help Desk</a></p>
+          <p><a href="https://itssc.rpi.edu/hc/en-us"target="_blank">IT Services</a></p> 
+          <p><a href="https://rpiathletics.com/"target="_blank">Athletics</a></p>
+          <p><a href="https://info.rpi.edu/comm-d"target="_blank">The Center for Global Communication + Design</a></p>
+        </div>
+      </div>
+      <div>
         <h1><u>Campus Map and Disorientation Guide</u></h1>
         <div className='pdfs'>
           <object
@@ -32,17 +46,6 @@ function OtherHelp() {
             Your browser does not support PDF viewing. You can view the disorientation guide PDF <a href="./PDFs/RPI_Disorientation_Guide.pdf">here</a>.
           </p>
           </object>
-        </div>
-      </div>
-
-      <div>
-      <h1><u>Other Useful Links</u></h1>
-        <div className='usefulLinks'> 
-          <p><a href="https://hasspathways.com/">Hass Pathways</a></p>
-          <p><a href="https://info.rpi.edu/campus-card/">Check your Flex/RAD Balance here</a></p>
-          <p><a href="https://dotcio.rpi.edu/services/support-and-training/help-desk">Help Desk</a></p>
-          <p><a href="https://itssc.rpi.edu/hc/en-us">IT Services</a></p> 
-          <p><a href="https://rpiathletics.com/">Athletics</a></p>
         </div>
       </div>
     </div>
